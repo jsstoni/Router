94cba13b1688bb33276c0d7c07becf6afd28298f
jsstoni <jsstoniha@gmail.com>
2023-11-24 13:38:04 -0300

    type

--- src/Request.php
+++ src/Request.php
@@ -4,9 +4,9 @@ namespace Route;
 
 final class Request
 {
-    public array $body;
-    public array $query;
-    public array $params;
-    public array $files;
+    public array | object $body;
+    public array | object $query;
+    public array | object $params;
+    public array | object $files;
     public ?string $authorization;
 }
